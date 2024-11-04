5aac42eb52770ae294cae1896d1894737b07f6ba
Bexruz Boynazarov <texnoxack@gmail.com>
2024-11-04 10:20:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from functools import partial
 from utils import get_last_seven_days
 import kundalikcom_func
 import os
-
+from android.permissions import request_permissions, Permission
 Builder.load_file("Login.kv")
 Builder.load_file("Home.kv")
 Builder.load_file("Datas.kv")
@@ -442,6 +442,7 @@ class RootWidget(ScreenManager):
 
 class KundalikCOMApp(App):
     def build(self):
+        request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
         self.database = DatabaseConnection()
         token = self.database.get_data("token")
         if token:
